c32d4c107c33f0200f930503a0c4a6d2332b238d
Skynin <sohay_ua@yahoo.com>
2018-03-07 14:34:16 +0200

    done

---
+++ tests/ParseNumberTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ParseNumberTest extends TestCase
+{
+
+	private function parseIt($a) : string
+	{
+		return preg_replace('/\.?[0,\s]*$/', '', $a);
+	}
+
+	/**
+     * @dataProvider additionProvider
+     * @return void
+     */
+    public function testBasicTest($a, $expected)
+    {
+       $this->assertSame($this->parseIt($a), $expected);
+    }
+
+	public function additionProvider ()
+	{
+		return [
+			['55.100', '55.1'],
+			['55.01', '55.01'],
+			['50.001', '50.001'],
+			['50.0010', '50.001'],
+			['50.0010 ', '50.001'],
+			['50.0010, ', '50.001'],
+			['55.00', '55']
+		];
+	}
+}
